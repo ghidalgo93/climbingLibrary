b8b0bf8438ff4e4eadfce223f4b76c3326e7eca9
Gerardo Hidalgo-Cuellar <ghidalgo93@gmail.com>
2020-10-21 17:18:04 -0600

    work on logic

--- myScript.js
+++ myScript.js
@@ -2,14 +2,19 @@
 // Author: Gerardo Hidalgo-Cuellar, github.com/ghidalgo93/climbingLibrary
 // Date: 10/21/20
 
+
+//****Variable Definitions****
+let climbLibrary = [];
+
+
 //****Object Definitions****
-function Climb(name, grade, location, date, sent, type) {
+function Climb(name, grade, location, date, type, sent) {
 	this.name = name;
 	this.grade = grade;
 	this.location = location;
 	this.date = date;
-	this.sent = sent;
 	this.type = type;
+	this.sent = sent;
 }
 
 
@@ -25,31 +30,52 @@ function addClimbToLibrary(library, climb) {
 
 // Updates a DOM table element with the given climbing library
 // input: DOM table, climbingLibrary
-function updateClimbLibrary(table, library){
+function displayClimbs(table, library){
 	//clear current table
-	removeAllChildNodes(table);
+	//removeAllChildNodes(table);
 	//loop through the library
-	//for each climb object --> create a new dom element 
-	//add that element to table
-}
-
-function removeAllChildNodes(parent){
-	while (parent.firstChild){
-		parent.removeChild(parent.firstChild);
+	for (const index in library){
+		//create an empty row element and add it to the last position of the table
+		let row = table.insertRow(0);
+		//insert cells into new row element
+		//add content to those cells
 	}
 }
 
-//****Variable Definitions****
-let climbLibrary = [];
-const climbTableBody = document.querySelector('#climbTablebody');
+function generateTableHead(table, data){
+	let thead = table.createTHead();
+	let row = thead.insertRow();
+	for (let key of data){
+		let th = document.createElement('th');
+		let text = document.createTextNode(key);
+		th.appendChild(text);
+		row.appendChild(th);
+	} 
+}
+
+function generateTable(table, data) {
+	for (let element of data) {
+		let row = table.insertRow();
+		for (key in element) {
+			let cell = row.insertCell();
+			let text = document.createTextNode(element[key]);
+			cell.appendChild(text);
+		}
+	}
+}
+
+//****DOM Definitions****
+let climbTable = document.querySelector('#climbTable');
 
 
 //****Script****
-const c1 = new Climb('american beauty', '5.12b', 'sport park', 'boulder canyon', '10/21/2020', true, 'sport');
-const c2 = new Climb('ten digit diling', '5.12d', 'wall of the 90s', 'clear creek', '10/20/2020', false, 'sport');
-
+const c1 = new Climb('american beauty', '5.12b', 'sport park', 'boulder canyon', '10/21/2020', 'sport', true);
+const c2 = new Climb('ten digit diling', '5.12d', 'wall of the 90s', 'clear creek', '10/20/2020', 'sport', false);
 
 addClimbToLibrary(climbLibrary, c1);
 addClimbToLibrary(climbLibrary, c2);
 
+let data = Object.keys(climbLibrary[0]);
+generateTable(climbTable, climbLibrary);
+generateTableHead(climbTable, data);
 
